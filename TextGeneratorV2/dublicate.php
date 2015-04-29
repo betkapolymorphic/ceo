e15978200ca41f73acb23611ab02933db22088d0
betkapolymorphic <a_albert@ukr.net>
2015-04-29 10:49:09 +0300

    alalized grups

--- TextGeneratorV2/dublicate.php
+++ TextGeneratorV2/dublicate.php
@@ -19,15 +19,15 @@ echo "loaded<br>";
 $p  =0;
 foreach($sentances  as $id=>$sentance){
 
-   if($p++>2000){
+   if($p++>20){
        break;
    }
     ///$c_words = preg_split('/,/',$sentance['id_words']);
-    $q = "SELECT id_propertie FROM ceoapp.word_propertie_relat where id_word in(".trim($sentance['id_words'],',').")";
+    $q = "SELECT id_propertie,group_concat(id_propertie) as gi FROM ceoapp.word_propertie_relat where id_word in(".trim($sentance['id_words'],',').") group by id_word";
     $res = mysql_query($q);
     $str = "";
     while($line = mysql_fetch_array($res,MYSQL_ASSOC)){
-        $str.=$line['id_propertie'].',';
+        $str.=$line['gi'].'|';
     }
     /*if(!isset($arr[$str])){
         $arr[$str]=0;
